f492bbaba187e2dbbf8b9ed6d9cba05d594d972b
Camila Nascimento Vasconcelos <camilavasconcelos715@gmail.com>
2026-01-30 14:53:47 +0000

    comandos inseridos char, int, float

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,4 +3,13 @@
 int main(){
   printf("desafio super trunfo!\n");
 
+    char nome[50];
+    int populacao;
+    int numero_de_pontos_turisticos;
+    float area;
+    float pib;
+    char codigo_da_carta;
+    char estado_da_cidade[50];
+
+
 } 
