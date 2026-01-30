51294828af8f9c4d54b26a48e67d6c982bd9af12
Camila Nascimento Vasconcelos <camilavasconcelos715@gmail.com>
2026-01-30 14:56:11 +0000

    inserido a entrada de dados do usuario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,5 +11,27 @@ int main(){
     char codigo_da_carta;
     char estado_da_cidade[50];
 
+     // 1. Entrada de dados do usuario - Usuario deve inserir os dados
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", nome);
+
+    printf("Digite o estado da cidade: \n");
+    scanf("%s", &estado_da_cidade);
+
+    printf("Digite a populacao da cidade: \n");
+    scanf("%d", &populacao);
+    
+    printf("Digite o numero de pontos turisticos da cidade: \n");
+    scanf("%d", &numero_de_pontos_turisticos);
+    
+    printf("Digite a area da cidade: \n");
+    scanf("%f", &area);
+   
+    printf("Digite o pib da cidade: \n");
+    scanf("%f", &pib);
+    
+    printf("Digite o codigo da carta da cidade: \n");
+    scanf(" %c", &codigo_da_carta);
+
 
 } 
